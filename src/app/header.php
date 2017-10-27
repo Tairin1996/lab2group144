4fc6a11d83923e1438607a0ac4281561201636df
Dominioncer <kushnirartyom@yandex.ru>
2017-10-28 03:34:29 +0500

    Styles fixes (reverted from commit ba87dc00059e8895233603bb64b23d374874bb1c)

---
+++ src/app/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <title><?php echo $menu[$module][$action]['title']; ?> - <?php echo $website;?> </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="College Education Responsive Template">
+    <meta name="author" content="Esmet">
+    <meta charset="UTF-8">
+
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- CSS Bootstrap & Custom -->
+    <link href=" ../vendor/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="../font/font-awesome.min.css" rel="stylesheet" media="screen">
+    <link href="../template/scss/animate.css" rel="stylesheet" media="screen">
+    <link href="../template/scss/style.css" rel="stylesheet" media="screen">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../font/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../font/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../font/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../font/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../font/ico/favicon.ico">
+
+    <!-- JavaScripts -->
+    <script src="../vendor/jquery-1.10.2.min.js"></script>
+    <script src="../vendor/jquery-migrate-1.2.1.min.js"></script>
+    <script src="../vendor/modernizr.js"></script>
+    <!--[if lt IE 8]>
+    <div style='clear:both; text-align:center; position:relative;'>
+        <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" alt="" /></a>
+    </div>
+    <![endif]-->
+</head>
